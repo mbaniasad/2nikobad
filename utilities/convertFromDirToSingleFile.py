68762ab1c170eb2964e88b61b5bbb1a7ae427695
mbaniasad <mb.sepanta@gmail.com>
2016-09-13 11:51:45 +0200

    utf8 encoding and using mopreprocessor

--- utilities/convertFromDirToSingleFile.py
+++ utilities/convertFromDirToSingleFile.py
@@ -1,10 +1,11 @@
-#this would also apply 
-#lower casing
-#removeing punctuation
+# -*- coding: utf-8 -*-
 
 import os
 import glob
 import string
+from motextpreprocessor import MoTextPreprocessor
+import codecs
+preprocessingConfig = dict(language = "de", stemming=False,removeUmlaut = True, lowerCase = True,removePunc=True)
 def readDatasFromDir(DirAddress,number_of_samples):
     sentences = []
     currdir = os.getcwd()
@@ -14,31 +15,30 @@ def readDatasFromDir(DirAddress,number_of_samples):
         sample_counter +=1
         if sample_counter > number_of_samples:
             break
-        with open(ff, 'r') as f:
+        with codecs.open(ff,'r',encoding='utf8') as f:
             sentences.append(f.readline().strip())
     os.chdir(currdir)
     return sentences
-datasetName="EAR-cls-acl10"
+datasetName="matelsoCalls-noUmlaut-nopunct"
 number_of_samples = 3000
-datasetAddress = "../2-DS/"+datasetName+"/"
-destinationDir = "../3-PREPROCESSED/LOWER-NOPUNC/"+datasetName+"-"+str(number_of_samples)+"Sample/"
+datasetAddress = "../3-PREPROCESSED/lower-nopunc-noumlaut/"+datasetName+"/"
+destinationDir = "../3-PREPROCESSED/lower-nopunc-noumlaut/"+datasetName+"/"
 if not os.path.exists(destinationDir):
     os.makedirs(destinationDir)
 
 
 trainPositive = [datasetAddress + "train/pos/", destinationDir+"train-pos.txt"]
 trainNegitive = [datasetAddress + "train/neg/", destinationDir+"train-neg.txt"]
-trainUnsuperviesd = [datasetAddress + "train/unsup/", destinationDir+"train-unsup.txt"]
+# trainUnsuperviesd = [datasetAddress + "train/unsup/", destinationDir+"train-unsup.txt"]
 testPositive = [datasetAddress + "test/pos/" ,destinationDir+"test-pos.txt"]
 testNegitive =[ datasetAddress + "test/neg/",destinationDir+"test-neg.txt"]
 
-addresses =[ trainPositive, trainNegitive,trainUnsuperviesd, testPositive, testNegitive]
+addresses =[ trainPositive, trainNegitive, testPositive, testNegitive]
 exclude = set(string.punctuation)
 
 for directoryAddress,filename in addresses:
-    with open (filename,'w') as destinationFile:
+    with open(filename,'w') as destinationFile:
         print(filename)
         for line in readDatasFromDir(directoryAddress,number_of_samples):
-            line = line.lower()
-            line = ''.join(ch for ch in line if ch not in exclude)
+            line =  MoTextPreprocessor.normalize(line,**preprocessingConfig).encode("utf-8")
             destinationFile.write(line+"\n")
